7de261f2a6a4d9918a9ed2600dc0ef43a0383126
midori-hust <phanthaobk@gmail.com>
2018-01-18 00:04:51 +0900

    3.Handle Call,send messenger

--- app/src/main/java/com/example/midori/phonebookapp/MainActivity.java
+++ app/src/main/java/com/example/midori/phonebookapp/MainActivity.java
@@ -1,12 +1,22 @@
 package com.example.midori.phonebookapp;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -39,7 +49,29 @@ public class MainActivity extends AppCompatActivity {
         initWidget();
         arrContact = new ArrayList<>();
         contactAdapter = new ContactAdapter(this,R.layout.activity_add_numberphone,arrContact);
+        checkAndRequestPermissions();
         lvContact.setAdapter(contactAdapter);
+        lvContact.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                showDialogConfirm(position);
+            }
+        });
+    }
+
+    public void checkAndRequestPermissions(){
+        String[] permissions = new String[]{Manifest.permission.CALL_PHONE,Manifest.permission.SEND_SMS};
+        List<String> listPermissions = new ArrayList<>();
+        //端末が許可をあげるかどうか？確認する？
+        for (String permission:permissions){
+            if (ContextCompat.checkSelfPermission(this,permission) != PackageManager.PERMISSION_GRANTED){
+                    listPermissions.add(permission);
+            }
+        }
+        //許可をもらいます？
+        if (!listPermissions.isEmpty()){
+            ActivityCompat.requestPermissions(this,listPermissions.toArray(new String[listPermissions.size()]),1);
+        }
     }
 
     public void initWidget(){
@@ -68,4 +100,38 @@ public class MainActivity extends AppCompatActivity {
             contactAdapter.notifyDataSetChanged();
         }
     }
+
+    public void showDialogConfirm(final int position){
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.activity_customer_dialog);
+        Button btn_call = (Button) dialog.findViewById(R.id.btn_call);
+        Button btn_messenger = (Button) dialog.findViewById(R.id.btn_messenger);
+        dialog.setTitle("Contact now!");
+        btn_call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               intentCall(position);
+            }
+        });
+
+        btn_messenger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intentSendMessenger(position);
+            }
+        });
+        dialog.show();
+    }
+
+    private void intentSendMessenger(int position){
+        Intent intent = new Intent(Intent.ACTION_VIEW,Uri.fromParts("sms",arrContact.get(position).getmNumber(),null));
+        startActivity(intent);
+    }
+    private void intentCall(int position) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:"+arrContact.get(position).getmNumber()));
+        startActivity(intent);
+    }
+
 }
